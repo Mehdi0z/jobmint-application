0c2ffd7dae41fb54b7e86ecee8ff4efbce4843eb
mehdi-jolly <mehdi@jollycommerce.io>
2023-08-17 19:55:29 +0100

    simple fix

--- assets/announcement-bar-script.js
+++ assets/announcement-bar-script.js
@@ -1,36 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     const countdowns = document.querySelectorAll('.announcement-bar__countdown')
 
-    if (!countdowns.length) {
-        return;
+    if(!countdowns.length) {
+        return; 
     }
 
     countdowns.forEach(countdown => {
+        
         const countdownTimeSpan = countdown.querySelector(".countdown-time");
         const { deadline, errorMessage } = countdown.dataset;
 
         if (countdownTimeSpan) {
-            // convert deadline to UTC
-            const deadlineUtc = new Date(deadline);
 
+            const deadlineDate = new Date(deadline);
+            const deadlineUtc = new Date(deadlineDate.getTime() + deadlineDate.getTimezoneOffset() * 60 * 1000);
+        
             function updateCountdown() {
-
-                // get difference between deadline & actual date (UTC)
-                const timeRemaining = deadlineUtc - new Date();
-
+                const now = new Date();
+                const nowUtc = new Date(now.getTime() + now.getTimezoneOffset() * 60 * 1000);
+                const timeRemaining = deadlineUtc - nowUtc;
+        
                 if (timeRemaining > 0) {
                     const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
                     const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
                     const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
+        
                     countdownTimeSpan.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
                 } else {
                     countdownTimeSpan.textContent = errorMessage;
                 }
             }
-
+        
             setInterval(updateCountdown, 1000);
         }
-    });
+
+    })
+
 });
